f4bfb71060a227f5c3c7b71325cb4ce59f37f119
informatica <informatica@drato.blumenau.ifc.edu.br>
2017-03-29 12:26:22 +0000

    criação das classe e implementação de linha

--- src/passagemaerea/Linha.java
+++ src/passagemaerea/Linha.java
@@ -1,20 +1,49 @@
 package passagemaerea;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Linha {
 
     static Scanner leia = new Scanner(System.in);
-    String AeroportoOrigem = "";
-    Str
-    public String pedeOrigemDestino() {
-        System.out.println("Informe o Aeroporto de embarque(Nome Aeroporto ou IATA):");
-        AeroportoOrigem = leia.nextLine();
-        return  
+
+    public String pedeOrigemDestino(String msg) {
+        System.out.println(msg);
+        String Aeroporto = leia.nextLine().trim();
+        return Aeroporto;
     }
 
     public String pedeData() {
-        return
+        String data;
+        Boolean verific;
+
+        DateFormat formatter = new SimpleDateFormat("dd/mm/yyyy");
+        Date dataInformada = null;
+        do {
+            System.out.println("Informe a data de embarque (dd/mm/aaaa)");
+            data = leia.nextLine();
+            try {
+                dataInformada = (Date) formatter.parse(data);
+                verific = false;
+            } catch (ParseException ex) {
+                System.out.println("seu burro, digite uma data válida.");
+                verific = true;
+            }
+        } while (verific);
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(dataInformada);
+        System.out.println("semana: " + c.getWeekYear());
+        return data;
+    }
+
+    public static void main(String[] args) {
+        Linha l = new Linha();
+        String s = l.pedeData();
     }
 
     public void calculaPrevisão() {
--- src/passagemaerea/Principal.java
+++ src/passagemaerea/Principal.java
@@ -1,14 +1,17 @@
-
 package passagemaerea;
 
-
 public class Principal {
-
    
+    static String AeroportoOrigem;
+    static String AeroportoDestino;
+    String msg = "Informe o aeroporto de";
+    
     public static void main(String[] args) {
+           Linha linha = new Linha();
+           linha.pedeOrigemDestino(msg);
+           AeroportoOrigem = linha.pedeOrigemDestino(AeroportoOrigem);
+           AeroportoDestino = linha.pedeOrigemDestino(AeroportoOrigem);
         
     }
-    
-    
-    
+
 }
